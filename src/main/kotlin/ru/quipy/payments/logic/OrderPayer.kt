3d7f9b9d815108fde72e2ec5cbcd4f45caee1e04
akp <apevkin@niuitmo.ru>
2025-12-17 18:42:36 +0300

    fix files again async

--- src/main/kotlin/ru/quipy/payments/logic/OrderPayer.kt
+++ src/main/kotlin/ru/quipy/payments/logic/OrderPayer.kt
@@ -1,7 +1,7 @@
 package ru.quipy.payments.logic
 
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.asCoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -13,6 +13,7 @@ import ru.quipy.common.utils.NamedThreadFactory
 import ru.quipy.common.utils.SlidingWindowRateLimiter
 import ru.quipy.core.EventSourcingService
 import ru.quipy.payments.api.PaymentAggregate
+import java.lang.Runnable
 import java.time.Duration
 import java.util.*
 import java.util.concurrent.LinkedBlockingQueue
@@ -47,12 +48,12 @@ class OrderPayer {
     private val processingTimeSec = 50L
 
     private val rateLimiter = SlidingWindowRateLimiter(rateLimitPerSec, Duration.ofSeconds(1))
-    fun processPayment(orderId: UUID, amount: Int, paymentId: UUID, deadline: Long): Long {
+    suspend fun processPayment(orderId: UUID, amount: Int, paymentId: UUID, deadline: Long): Long {
 
-        val toBlock = deadline - System.currentTimeMillis()
-        if (!rateLimiter.tickBlocking(Duration.ofMillis(toBlock))) {
-            throw RuntimeException("Rate limit exceeded")
-        }
+//        val toBlock = deadline - System.currentTimeMillis()
+//        if (!rateLimiter.tickBlocking(Duration.ofMillis(toBlock))) {
+//            throw RuntimeException("Rate limit exceeded")
+//        }
 //
 //        if (toBlock <= 0) {
 //            throw TooManyRequestsError(1000)
@@ -63,13 +64,24 @@ class OrderPayer {
 //        }
 
         val createdAt = System.currentTimeMillis()
-        paymentExecutor.submit {
-            val createdEvent = paymentESService.create {
-                it.create(paymentId, orderId, amount)
+//        paymentExecutor.submit {
+//            val createdEvent = paymentESService.create {
+//                it.create(paymentId, orderId, amount)
+//            }
+//            logger.trace("Payment ${createdEvent.paymentId} for order $orderId created.")
+//            paymentService.submitPaymentRequest(paymentId, amount, createdAt, deadline)
+//        }
+        GlobalScope.launch(Dispatchers.IO) {
+            try {
+                paymentESService.create {
+                    it.create(paymentId, orderId, amount)
+                }
+                logger.trace("Payment $paymentId for order $orderId created.")
+            } catch (e: Exception) {
+                logger.error("Error creating payment $paymentId for order $orderId", e)
             }
-            logger.trace("Payment ${createdEvent.paymentId} for order $orderId created.")
-            paymentService.submitPaymentRequest(paymentId, amount, createdAt, deadline)
         }
+        paymentService.submitPaymentRequest(paymentId, amount, createdAt, deadline)
         return createdAt
     }
 }
