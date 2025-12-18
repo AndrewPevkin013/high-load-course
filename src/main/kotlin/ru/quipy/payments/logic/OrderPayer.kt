1963982fd210f429062b1e86925e05c67f611187
akp <apevkin@niuitmo.ru>
2025-12-19 00:25:55 +0300

    test maxSize of pools

--- src/main/kotlin/ru/quipy/payments/logic/OrderPayer.kt
+++ src/main/kotlin/ru/quipy/payments/logic/OrderPayer.kt
@@ -34,8 +34,8 @@ class OrderPayer {
     private lateinit var paymentService: PaymentService
     private val paymentExecutor = ThreadPoolExecutor(
         200,
-        400, // пропускная способность одного потока 1/averageProccesingTime = 1/0,5 = 2 , rps = 100 , 100/2 = 50
-        60L,
+        1100, // пропускная способность одного потока 1/averageProccesingTime = 1/0,5 = 2 , rps = 100 , 100/2 = 50
+        0L,
         TimeUnit.MILLISECONDS,
         LinkedBlockingQueue(10_000),
         NamedThreadFactory("payment-submission-executor"),
