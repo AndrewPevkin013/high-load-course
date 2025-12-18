27ae240db3d2ac0aeceeb7addeda176f4ab39ea0
akp <apevkin@niuitmo.ru>
2025-12-19 00:44:31 +0300

    fix params again for test

--- src/main/kotlin/ru/quipy/payments/logic/OrderPayer.kt
+++ src/main/kotlin/ru/quipy/payments/logic/OrderPayer.kt
@@ -34,10 +34,10 @@ class OrderPayer {
     private lateinit var paymentService: PaymentService
     private val paymentExecutor = ThreadPoolExecutor(
         32,
-        32, // пропускная способность одного потока 1/averageProccesingTime = 1/0,5 = 2 , rps = 100 , 100/2 = 50
+        64, // пропускная способность одного потока 1/averageProccesingTime = 1/0,5 = 2 , rps = 100 , 100/2 = 50
         0L,
         TimeUnit.MILLISECONDS,
-        LinkedBlockingQueue(8_000),
+        LinkedBlockingQueue(20_000),
         NamedThreadFactory("payment-submission-executor"),
         CallerBlockingRejectedExecutionHandler()
     )
