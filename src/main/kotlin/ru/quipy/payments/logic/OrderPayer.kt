0a7ad8206bc4367fb92841978e1316b696904e66
akp <apevkin@niuitmo.ru>
2025-12-19 17:52:39 +0300

    last test

--- src/main/kotlin/ru/quipy/payments/logic/OrderPayer.kt
+++ src/main/kotlin/ru/quipy/payments/logic/OrderPayer.kt
@@ -33,11 +33,11 @@ class OrderPayer {
     @Autowired
     private lateinit var paymentService: PaymentService
     private val paymentExecutor = ThreadPoolExecutor(
-        40,
-        40, // пропускная способность одного потока 1/averageProccesingTime = 1/0,5 = 2 , rps = 100 , 100/2 = 50
+        32,
+        32, // пропускная способность одного потока 1/averageProccesingTime = 1/0,5 = 2 , rps = 100 , 100/2 = 50
         0L,
         TimeUnit.MILLISECONDS,
-        LinkedBlockingQueue(20_000),
+        LinkedBlockingQueue(10_000),
         NamedThreadFactory("payment-submission-executor"),
         CallerBlockingRejectedExecutionHandler()
     )
@@ -55,11 +55,11 @@ class OrderPayer {
         val toBlock = deadline - System.currentTimeMillis()
 
         if (!rateLimiter.tick()) {
-            throw TooManyRequestsError(1_000)
+            throw TooManyRequestsError(10_000)
         }
 
         if (toBlock <= 0) {
-            throw TooManyRequestsError(1_000)
+            throw TooManyRequestsError(10_000)
         }
 
         val createdAt = System.currentTimeMillis()
