03fbd9242af3d1ee901d7ac00a0f501d76b845e5
akp <apevkin@niuitmo.ru>
2025-12-26 16:29:11 +0300

    ну пройди4

--- src/main/kotlin/ru/quipy/payments/logic/OrderPayer.kt
+++ src/main/kotlin/ru/quipy/payments/logic/OrderPayer.kt
@@ -32,8 +32,8 @@ class OrderPayer {
     @Autowired
     private lateinit var paymentService: PaymentService
     private val paymentExecutor = ThreadPoolExecutor(
-        64,
-        64,
+        32,
+        32,
         0L,
         TimeUnit.MILLISECONDS,
         LinkedBlockingQueue(10_000),
