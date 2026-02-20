6653d86cbb19eca3ea94b1649c44a8057c0c05e8
akp <apevkin@niuitmo.ru>
2026-02-20 11:57:01 +0300

    fix params 15

--- src/main/kotlin/ru/quipy/payments/logic/OrderPayer.kt
+++ src/main/kotlin/ru/quipy/payments/logic/OrderPayer.kt
@@ -44,9 +44,9 @@ class OrderPayer {
     private val executorScope = CoroutineScope(paymentExecutor.asCoroutineDispatcher())
 
     private val rateLimiter = LeakingBucketRateLimiter(
-        rate = 1100,
+        rate = 4000,
         window = Duration.ofMillis(1000),
-        bucketSize = 20000
+        bucketSize = 40000
     )
 
     suspend fun processPayment(orderId: UUID, amount: Int, paymentId: UUID, deadline: Long): Long {
